c534aba0e5d7d1f2879c8417e93b45e032c69d07
hisyamidris <akmal.idris@colorado.edu>
2014-12-07 14:35:05 -0700

    Added Oldman Assets & Interactions
    - Oldman will now be able to push objects
    - Collider around the old man will check for velocity of incoming
    objects (if you get hit -> check object velocity)

---
+++ Project/Assets/Scripts/objectInteraction.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectInteraction : MonoBehaviour {
+
+	public float pushPower = 2.0F;
+
+	// Use this for initialization
+	void Start() {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other) {
+		if(Input.GetKey(KeyCode.I) && other.gameObject.tag == "Pushable")
+		if(other.rigidbody != null)
+			other.rigidbody.AddForce ((other.transform.position - transform.parent.position).normalized * pushPower);
+	}
+
+
+
+}
