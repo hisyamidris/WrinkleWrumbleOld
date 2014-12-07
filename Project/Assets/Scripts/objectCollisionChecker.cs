05eab74e91c29e80600acb52f1f79b290a1106cb
hisyamidris <akmal.idris@colorado.edu>
2014-12-07 14:36:17 -0700

    Updated CollisionChecker

---
+++ Project/Assets/Scripts/objectCollisionChecker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectCollisionChecker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Pushable")
+				if (other.rigidbody != null)
+						Debug.Log (other.rigidbody.velocity.magnitude);
+	}
+}
